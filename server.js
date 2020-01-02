f15a6f5e3423ffd84834b17b054a7333f43f0624
Versifiction <marc_charpentier@hotmail.fr>
2020-01-02 18:55:10 +0100

    add files

--- server.js
+++ server.js
@@ -1,20 +1,139 @@
-const path = require("path");
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
+const users = require("./routes/api/users");
+const chat = require("./routes/api/chat");
+const date = require("./routes/api/date");
+const root = require("./routes/api/root");
+const ObjectId = require("mongodb").ObjectId;
+const db = require("./config/keys").mongoURI;
 const port = process.env.PORT || 5000;
+const app = express();
+const server = app.listen(5000);
+const io = require("socket.io").listen(server);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Bienvenue sur l'API O'Films" });
+require("dotenv").config();
+require("./config/passport")(passport);
+
+app.use(morgan("tiny"));
+
+if (process.env.NODE_ENV === "production") {
+  console.log("PROD ENV ", process.env.NODE_ENV);
+
+  app.use(express.static("build"));
+  app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
+}
+// else if (process.env.NODE_ENV === "development") {
+//   console.log("DEV ENV ", process.env.NODE_ENV);
+//   app.use(express.static("public"));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve("public", "index.html"))
+//   );
+// }
+
+var whitelist = [
+  process.env.CLIENT_PORT,
+  process.env.CLIENT_PRODUCTION,
+  process.env.SERVER_PORT
+];
+
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use("/api/users", users);
+app.use("/api/chat", chat);
+app.use("/api/date", date);
+app.use("/api/root", root);
+
+let Message = require("./models/ChatMessage");
+let typingusers = [];
+
+io.on("connection", function(socket) {
+  socket.on("send message", async function(message) {
+    try {
+      console.log("message envoyé");
+      io.emit("send message", message);
+      const sent = await Message.create(message, function(err, res) {
+        if (err) throw err;
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  socket.on("delete message", async function(message) {
+    try {
+      console.log("message supprimé");
+      io.emit("delete message", message);
+      const id = message.id;
+      const o_id = new ObjectId(id);
+      const deleted = await Message.findOneAndDelete({ _id: o_id }, function(
+        err,
+        user
+      ) {
+        if (err) {
+          throw err;
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  function broadcastTyping() {
+    let message = "";
+    if (typingusers.length === 1) {
+      message = typingusers[0] + " est en train d'écrire";
+    } else if (typingusers.length === 2) {
+      let typingusersAsArray = Object.values(typingusers);
+      message = typingusersAsArray.join("et ") + " sont en train d'écrire";
+    } else if (typingusers.length > 2) {
+      message = "Plusieurs personnes sont en train d'écrire";
+    }
+
+    io.emit("typing message", { message, typingusers });
+  }
+
+  socket.on("typing message", username => {
+    if (!typingusers.includes(username.username)) {
+      typingusers.push(username.username);
+    }
+    broadcastTyping();
+  });
+
+  socket.on("not typing message", username => {
+    typingusers.splice(typingusers.indexOf(username.username), 1);
+    broadcastTyping();
+  });
 });
 
-app.get("/message", (req, res) => {
-  res.json({ message: "Test déploiement d'O'Films sur Heroku" });
-});
-
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello world !" });
-});
-
-app.listen(port, () => {
-  console.log(`Server is on up ${port} now !`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@ofilms-demo-f9iwz.mongodb.net/${process.env.DB}`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() =>
+    console.log(
+      "Le serveur tourne sur le port 5000 et la connexion à la base de données MongoDB s'est bien déroulée"
+    )
+  )
+  .catch(err => console.log(err));
